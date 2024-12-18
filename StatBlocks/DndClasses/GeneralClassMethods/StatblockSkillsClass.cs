59a61fd4852a28261f2869690458a3dc720bc8e1
Bruno Feist <b22.feist@student.stjohns-hamilton.school.nz>
2024-12-18 18:35:22 +1300

    finished subrace skills method which adds skills characters receive from being at specific subrace.

--- StatBlocks/DndClasses/GeneralClassMethods/StatblockSkillsClass.cs
+++ StatBlocks/DndClasses/GeneralClassMethods/StatblockSkillsClass.cs
@@ -8,14 +8,17 @@ public class StatblockProfenciesClass
 	/*stat block skills class handles proficiencies and proficiency bonus for a stat block
 	 it takes an argument of background, class, race and level
 	 it handles saving throws, skills, tools, weapons and armour proficiencies and languages */
-	public Dictionary<string, object> ProfenciesMethod(string background, List<string> dndclass, string race, int level)
+	public Dictionary<string, object> ProfenciesMethod(string background, List<string> dndclass, string race, List<int> level)
 	{
-		Dictionary<string, object> Proficiencies = new();
+		Dictionary<string, object> Proficiencies = new()
+		{
+			{"skills", SkillsGeneratorMethod(background, dndclass, race, level) }
+		};
 		return Proficiencies;
 	}
 
 	private Dictionary<string, int> SkillsGeneratorMethod(string background, List<string> dndclass, string race,
-		int level)
+		List<int> level)
 	{
 		ImmutableArray<string> Skills =
 		[
@@ -24,6 +27,78 @@ public class StatblockProfenciesClass
 			"sleight of hand", "stealth", "survival"
 		];
 		
+		int TotalLevel = 0;
+
+		foreach (int LevelNumbers in level)
+		{
+			TotalLevel += LevelNumbers;
+		}
+		ImmutableArray<int> ProfBonusArray = [2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 6, 6, 6, 6];
+
+		int ProfBonus = ProfBonusArray[TotalLevel];
 		
+		Dictionary<string, int> StatblockSkillsDictionary = new();
+		
+		return StatblockSkillsDictionary;
+		}
+
+	private List<string> SubraceSkills(string race)
+	{
+		List<string> SubraceSkills = new();
+		ImmutableArray<string> Skills =
+		[
+			"acrobatics", "animal handling", "arcana", "athletics", "deception", "history", "insight", "intimidation",
+			"investigation", "medicine", "nature", "perception", "performance", "persuasion", "religion",
+			"sleight of hand", "stealth", "survival"
+		];
+
+		switch (race.ToLower())
+		{
+			case "variant human":
+				foreach (string SkillsStrings in Skills)
+				{
+					Console.WriteLine(SkillsStrings);
+				}		
+				
+				for (int VariantHumanSkillsCounter = 0; VariantHumanSkillsCounter < 2; VariantHumanSkillsCounter++)
+				{
+					Console.WriteLine("enter in a skills");
+					string? UserRaceSkillsinput = Console.ReadLine();
+
+					if (Skills.Contains(UserRaceSkillsinput.ToLower()))
+					{
+						SubraceSkills.Add(UserRaceSkillsinput.ToLower());
+					}
+				}
+				break;
+			
+			case "half elf":
+				foreach (string SkillsStrings in Skills)
+				{
+					Console.WriteLine(SkillsStrings);
+				}		
+				
+				for (int HalfElfSkillsCounter = 0; HalfElfSkillsCounter < 2; HalfElfSkillsCounter++)
+				{
+					Console.WriteLine("enter in a skills");
+					string? UserRaceSkillsinput = Console.ReadLine();
+
+					if (Skills.Contains(UserRaceSkillsinput.ToLower()))
+					{
+						SubraceSkills.Add(UserRaceSkillsinput.ToLower());
+					}
+				}
+				break;
+			
+			case "half orc":
+				SubraceSkills.Add("intimidation");
+				break;
+			
+			case "elf":
+				SubraceSkills.Add("perception");
+				break;
+		}
+
+		return SubraceSkills;
 	}
 }
